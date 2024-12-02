9efe303dee562dc13c93bf356bde9293715e0a7c
tnanhd <tnanh.dev@gmail.com>
2024-12-02 21:37:11 +0700

    Feat: Use LiveJournalService

--- TripJournal/RootView.swift
+++ TripJournal/RootView.swift
@@ -6,14 +6,12 @@ struct RootView: View {
     @State private var addAction: () -> Void = {}
     @State private var isAuthenticated = false
 
-    @Environment(\.journalService) private var journalService
-
     // MARK: - Body
 
     var body: some View {
         content
-            .environment(\.journalService, journalService)
-            .onReceive(journalService.isAuthenticated.receive(on: DispatchQueue.main)) { isAuthenticated in
+            .environment(\.journalService, service)
+            .onReceive(service.isAuthenticated.receive(on: DispatchQueue.main)) { isAuthenticated in
                 self.isAuthenticated = isAuthenticated
             }
     }
